4b7a486cb6cc7b859487ed1ba6feb00f59cbeb71
kushal-sinha <kushal147258369@gmail.com>
2024-10-10 04:45:31 +0530

    custom signup and signin

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react'
+
+interface DashboardLayoutProps {
+    children: ReactNode
+}
+const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
+    return (
+        <div>
+            {children}
+        </div>
+    );
+};
+
+export default DashboardLayout;
