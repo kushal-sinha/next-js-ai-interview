b4bb427af3e94900dc3d6f37d0d14acf3c210e5f
kushal-sinha <kushal147258369@gmail.com>
2024-10-15 16:13:55 +0530

    refactored

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/', // Redirect from the root path
+                destination: '/dashboard', // Redirect to the dashboard
+                permanent: true, // Use a 308 redirect (permanent)
+            },
+        ];
+    },
+};
 
 export default nextConfig;
